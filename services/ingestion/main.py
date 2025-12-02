08139f3cc8403e77fdaedb42f9a08275bc2f22c3
RDsanchez-git <rdsanchez.econ@gmail.com>
2025-12-01 22:48:31 -0300

    Mejoras finales en el ETL. Comienzo del Análisis EDA
    Se realizaron cambios para aumentar la rigurosidad del proceso ETL y se añadió una notebook para realizar el análisis EDA de la base de datos.
    Estos arreglos hicieron que se deban cambiar varios archivos, como el del ETL, el main.py (ingesta de datos a SQL).
    Se añadieron comentarios en los notebooks para que sea entendible todo lo que se hizo.
    
    En relación al análisis EDA, está en proceso de construcción.

--- services/ingestion/main.py
+++ services/ingestion/main.py
@@ -17,34 +17,31 @@ engine = create_engine(connection_string)
 DATA_DROP_PATH = "/app/data_drop"
 
 def load_clean_file(csv_path):
-    print(f"--> Ingestando archivo ya normalizado: {os.path.basename(csv_path)}")
+    print(f"--> Ingestando archivo Maestro V6: {os.path.basename(csv_path)}")
     
     try:
         df = pd.read_csv(csv_path)
         
-        # VALIDACIÓN: Verificar si las columnas ya están en formato DB (minúsculas)
-        # Si el usuario corrió el Notebook 00 correctamente, 'Player' ya debería ser 'player_name'
-        if 'player_name' not in df.columns and 'Player' in df.columns:
-            print("⚠️ ADVERTENCIA: El archivo parece no tener los nombres estandarizados.")
-            print("   Por favor, ejecuta el Notebook 00 actualizado.")
+        # Validación de Seguridad
+        if 'player_name' not in df.columns:
+            print("❌ Error: El archivo no parece tener el formato Elite (falta 'player_name').")
             return
 
-        # Asegurar season_id
+        # Metadata extra si falta
         if 'season_id' not in df.columns:
             df['season_id'] = 2024
 
-        # CARGA DIRECTA
-        print(f"   -> Insertando {len(df)} registros...")
-        # 'replace' es seguro aquí porque queremos que la tabla sea idéntica al CSV limpio
+        print(f"   -> Insertando {len(df)} registros con {len(df.columns)} columnas...")
+        
+        # CARGA DIRECTA (Porque el Notebook ya hizo todo el trabajo sucio)
         df.to_sql('player_performance_real', con=engine, if_exists='replace', index=False)
         print("   ✅ Carga exitosa.")
         
     except Exception as e:
-        print(f"   ❌ Error cargando {csv_path}: {e}")
+        print(f"   ❌ Error cargando: {e}")
 
 def main():
-    print(">>> INICIANDO LOADER V4 (DIRECT LOAD) <<<")
-    
+    print(">>> INICIANDO LOADER FINAL (V6) <<<")
     files = glob.glob(f"{DATA_DROP_PATH}/*_clean.csv")
     
     if not files:
